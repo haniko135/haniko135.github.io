af73383429ba1357e5cb1c7db05a4186dc39a046
haniko135 <70800638+haniko135@users.noreply.github.com>
2020-12-03 00:18:27 +0300

    update

--- page 1/ver 1.1/pages with info about stories (ver 1.1) light/info_pages_script.js
+++ page 1/ver 1.1/pages with info about stories (ver 1.1) light/info_pages_script.js
@@ -1,34 +1,68 @@
 let j = 0;
 function openNav() {
-    if(j == 0){
-        document.getElementById("mySidenav").style.width = "250px";
-        document.body.style.backgroundColor = "rgba(157,157,157,0.7)";
-        document.getElementById("searchIcon").style.backgroundColor = "#ceba85";
-        j++;
+    if(document.documentElement.clientWidth > 700){
+        if(j == 0){
+            document.getElementById("mySidenav").style.width = "25%";
+            document.body.style.backgroundColor = "rgba(157,157,157,0.7)";
+            document.getElementById("searchIcon").style.backgroundColor = "#ceba85";
+            j++;
+        }
+        else if(j == 1){
+            document.getElementById("mySidenav").style.width = "0";
+            document.body.style.backgroundColor = "#dfdfdf";
+            document.getElementById("searchIcon").style.backgroundColor = "#dfdfdf";
+            j--;
+        }
     }
-    else if(j == 1){
-        document.getElementById("mySidenav").style.width = "0";
-        document.body.style.backgroundColor = "#dfdfdf";
-        document.getElementById("searchIcon").style.backgroundColor = "#dfdfdf";
-        j--;
+    else if(document.documentElement.clientWidth <= 700){
+        if(j == 0){
+            document.getElementById("mySidenav").style.width = "45%";
+            document.body.style.backgroundColor = "rgba(157,157,157,0.7)";
+            document.getElementById("searchIcon").style.backgroundColor = "#ceba85";
+            j++;
+        }
+        else if(j == 1){
+            document.getElementById("mySidenav").style.width = "0";
+            document.body.style.backgroundColor = "#dfdfdf";
+            document.getElementById("searchIcon").style.backgroundColor = "#dfdfdf";
+            j--;
+        }
     }
 }
 
 let i = 0;
 function dropMenu(){
-    if(i == 0){
-        document.getElementById("pageMenu").style.width="9.5%";
-        document.body.style.backgroundColor = "rgba(157,157,157,0.7)";
-        document.getElementById("menuIcon").style.backgroundColor = "rgba(86, 86, 86, 0.01)";
-        document.getElementById("menuBar").style.backgroundColor = "rgba(86, 86, 86, 0.01)";
-        i++;
+    if(document.documentElement.clientWidth > 700){
+        if(i == 0){
+            document.getElementById("pageMenu").style.width="9.5%";
+            document.body.style.backgroundColor = "rgba(157,157,157,0.7)";
+            document.getElementById("menuIcon").style.backgroundColor = "rgba(86, 86, 86, 0.01)";
+            document.getElementById("menuBar").style.backgroundColor = "rgba(86, 86, 86, 0.01)";
+            i++;
+        }
+        else if(i == 1){
+            document.getElementById("pageMenu").style.width = "0";
+            document.body.style.backgroundColor = "#dfdfdf";
+            document.getElementById("menuIcon").style.backgroundColor = "#857154";
+            document.getElementById("menuBar").style.backgroundColor = "#857154";
+            i--;
+        }
     }
-    else if(i == 1){
-        document.getElementById("pageMenu").style.width = "0";
-        document.body.style.backgroundColor = "#dfdfdf";
-        document.getElementById("menuIcon").style.backgroundColor = "#857154";
-        document.getElementById("menuBar").style.backgroundColor = "#857154";
-        i--;
+    else if(document.documentElement.clientWidth <= 700){
+        if(i == 0){
+            document.getElementById("pageMenu").style.width="19.5%";
+            document.body.style.backgroundColor = "rgba(157,157,157,0.7)";
+            document.getElementById("menuIcon").style.backgroundColor = "rgba(86, 86, 86, 0.01)";
+            document.getElementById("menuBar").style.backgroundColor = "rgba(86, 86, 86, 0.01)";
+            i++;
+        }
+        else if(i == 1){
+            document.getElementById("pageMenu").style.width = "0";
+            document.body.style.backgroundColor = "#dfdfdf";
+            document.getElementById("menuIcon").style.backgroundColor = "#857154";
+            document.getElementById("menuBar").style.backgroundColor = "#857154";
+            i--;
+        }
     }
 }
 
@@ -51,3 +85,35 @@ function searchOnPage() {
         }
     }
 }
+
+let tooltipElem;
+document.onmouseover = function(event) {
+    let focus = event.target;
+
+    let tooltipHtml = focus.dataset.tooltip;
+    if (!tooltipHtml) return;
+
+    tooltipElem = document.createElement('div');
+    tooltipElem.className = 'tooltip';
+    tooltipElem.innerHTML = tooltipHtml;
+    document.body.append(tooltipElem);
+    
+    let coords = focus.getBoundingClientRect();
+
+    let right = coords.right - (focus.offsetWidth - tooltipElem.offsetWidth) / 2;
+    if (right < 0) { right = 0; } // не заезжать за левый край окна
+
+    let top = coords.top + tooltipElem.offsetHeight - 30;
+    if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+        top = coords.top + focus.offsetHeight;
+    }
+    tooltipElem.style.left = right + 'px';
+    tooltipElem.style.top = top + 'px';
+};
+
+document.onmouseout = function(e) {
+    if (tooltipElem) {
+        tooltipElem.remove();
+        tooltipElem = null;
+    }
+};
